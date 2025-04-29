eadf3ed50c7d9228987987df7d38d0b688fdab6e
iamsantoshku <santoshkr1592000@gmail.com>
2025-04-29 14:58:42 +0530

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,77 +1,40 @@
-// import React from 'react'
-// import Herobanner from '../component/home/Herobanner'
-// import Herosearch from '../component/home/Herosearch'
-// import Toursoffer from '../component/home/Toursoffer'
-// import Popularat from '../component/home/Popularat'
+import React from 'react'
+import Herobanner from '../component/home/Herobanner'
+import Herosearch from '../component/home/Herosearch'
+import Toursoffer from '../component/home/Toursoffer'
+import Popularat from '../component/home/Popularat'
 
-// import VideoHelp from '../component/home/Vidiohelp'
-// import Review from '../component/home/Review'
-// import Exprience from '../component/home/Exprience'
-// import Calltoaction from '../component/home/Calltoaction'
-// import Hoteldet from '../component/hotel/Hoteldet'
-// import Hoteldethome from '../component/home/Hoteldethome'
-
-// const Home = () => {
-//   return (
-//     <div>
-//         <Herobanner/>
-//         <Herosearch/>
-//         <Toursoffer/>
-//         <Popularat/>
-        
-//         {/* <Hoteldet/> */}
-//         <Hoteldethome/>
-//         {/* <Location/> */}
-        
-//         {/* <VideoHelp/> */}
-//         <Review/>
-//         <Exprience/>
-//         <Calltoaction/>
-      
-//     </div>
-//   )
-// }
-
-// export default Home
-
-
-
-
-
-import React, { Suspense, lazy } from 'react'
-
-// Lazy loading components
-const Herobanner = lazy(() => import('../component/home/Herobanner'))
-const Herosearch = lazy(() => import('../component/home/Herosearch'))
-const Toursoffer = lazy(() => import('../component/home/Toursoffer'))
-const Popularat = lazy(() => import('../component/home/Popularat'))
-const Hoteldethome = lazy(() => import('../component/home/Hoteldethome'))
-const Review = lazy(() => import('../component/home/Review'))
-const Exprience = lazy(() => import('../component/home/Exprience'))
-const Calltoaction = lazy(() => import('../component/home/Calltoaction'))
-// const VideoHelp = lazy(() => import('../component/home/Vidiohelp'))
-// const Hoteldet = lazy(() => import('../component/hotel/Hoteldet'))
+import VideoHelp from '../component/home/Vidiohelp'
+import Review from '../component/home/Review'
+import Exprience from '../component/home/Exprience'
+import Calltoaction from '../component/home/Calltoaction'
+import Hoteldet from '../component/hotel/Hoteldet'
+import Hoteldethome from '../component/home/Hoteldethome'
 
 const Home = () => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Herobanner />
-        <Herosearch />
-        <Toursoffer />
-        <Popularat />
+        <Herobanner/>
+        <Herosearch/>
+        <Toursoffer/>
+        <Popularat/>
         
         {/* <Hoteldet/> */}
-        <Hoteldethome />
+        <Hoteldethome/>
         {/* <Location/> */}
         
         {/* <VideoHelp/> */}
-        <Review />
-        <Exprience />
-        <Calltoaction />
-      </Suspense>
+        <Review/>
+        <Exprience/>
+        <Calltoaction/>
+      
     </div>
   )
 }
 
 export default Home
+
+
+
+
+
