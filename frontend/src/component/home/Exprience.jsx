46a1380e910b72e483390e400151df0e61f0a0b5
iamsantoshku <santoshkr1592000@gmail.com>
2025-04-29 16:24:38 +0530

    herobanner

--- frontend/src/component/home/Exprience.jsx
+++ frontend/src/component/home/Exprience.jsx
@@ -4,40 +4,36 @@
 import React from "react";
 
 const Exprience = () => {
-  const description = `Traveling creates unforgettable memories—waking up to a sunrise over the mountains, the salty breeze of an untouched beach, or the laughter of new friends in a foreign land. It’s the joy of discovering hidden alleys, tasting exotic cuisines, and capturing breathtaking landscapes. Every journey tells a story—getting lost in vibrant city streets, dancing under the northern lights, or sharing moments with locals who turn into lifelong friends. Travel isn’t just about places; it’s about experiences that shape us, broaden our minds, and leave footprints on our hearts. The best memories aren’t planned; they’re found in the unexpected adventures.`;
-
   return (
     <div>
       <section>
         <div className="container">
           <div className="row align-items-center justify-content-between">
+
             {/* Image Section */}
             <div className="col-xl-5 col-lg-5 col-md-6">
               <div className="position-relative">
                 <img
                   src="/assets/img/img-2.webp"
-                  loading="lazy"
                   className="img-fluid rounded-3 position-relative z-1"
                   alt="Experience"
                 />
                 <div className="position-absolute bottom-0 start-0 z-index-1 mb-4 ms-2">
                   <div className="bg-body d-flex d-inline-block rounded-3 position-relative p-3 z-2 shadow-wrap">
-
+                    
                     {/* Avatar Group */}
                     <div className="me-4">
                       <h6 className="fw-normal">Client</h6>
                       <ul className="avatar-group mb-0">
                         <li className="avatar avatar-md">
                           <img
-                            loading="lazy"
                             className="avatar-img circle"
-                            src="/assets/img/team-1.webp"
+                            src="assets/img/team-1.webp"
                             alt="avatar"
                           />
                         </li>
                         <li className="avatar avatar-md">
                           <img
-                            loading="lazy"
                             className="avatar-img circle"
                             src="/assets/img/team-2.webp"
                             alt="avatar"
@@ -45,7 +41,6 @@ const Exprience = () => {
                         </li>
                         <li className="avatar avatar-md">
                           <img
-                            loading="lazy"
                             className="avatar-img circle"
                             src="/assets/img/team-3.webp"
                             alt="avatar"
@@ -53,7 +48,6 @@ const Exprience = () => {
                         </li>
                         <li className="avatar avatar-md">
                           <img
-                            loading="lazy"
                             className="avatar-img circle"
                             src="/assets/img/team-4.webp"
                             alt="avatar"
@@ -76,7 +70,6 @@ const Exprience = () => {
                         4.5<i className="fa-solid fa-star text-warning ms-1"></i>
                       </h6>
                     </div>
-
                   </div>
                 </div>
               </div>
@@ -91,7 +84,9 @@ const Exprience = () => {
                 <h2 className="fw-bold lh-base">
                   Our Attractive Experience And Services For You!
                 </h2>
-                <p className="fw-light fs-6">{description}</p>
+                <p className="fw-light fs-6">
+                Traveling creates unforgettable memories—waking up to a sunrise over the mountains, the salty breeze of an untouched beach, or the laughter of new friends in a foreign land. It’s the joy of discovering hidden alleys, tasting exotic cuisines, and capturing breathtaking landscapes. Every journey tells a story—getting lost in vibrant city streets, dancing under the northern lights, or sharing moments with locals who turn into lifelong friends. Travel isn’t just about places; it’s about experiences that shape us, broaden our minds, and leave footprints on our hearts. The best memories aren’t planned; they’re found in the unexpected adventures
+                </p>
                 <div className="d-inline-flex mt-4">
                   <div className="d-inline-flex flex-column justify-content-center align-items-center py-3 px-3 rounded gray-simple me-3">
                     <h6 className="fw-bold fs-3 m-0">33</h6>
